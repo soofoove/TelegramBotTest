3b7add935719fa1bae260ee7c9e49379424369a0
Mike Ryzhov <pe4enkus@gmail.com>
2018-10-19 01:50:19 +0300

    basic functionality

--- Extensions/ServiceExtensions.cs
+++ Extensions/ServiceExtensions.cs
@@ -1,12 +1,18 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 using Telegram.Bot;
 
 namespace webApp.Extensions{
     public static class ServiceExtensions{
-        public static IServiceCollection AddTelegramBot(this IServiceCollection services, string token){
+        public static IServiceCollection AddTelegramBot(this IServiceCollection services){
             return services.AddSingleton<TelegramBotClient>(provider => {
-                return new TelegramBotClient(token);
+                var config = provider.GetService<IConfiguration>();
+                
+                var client = new TelegramBotClient(config["botToken"]);
+                client.DeleteWebhookAsync();
+                client.SetWebhookAsync(config["webhook_url"]);
+                return client;
             });
         }
     }
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,31 +8,57 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
 
 using webApp.Extensions;
 
 using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+using Newtonsoft.Json;
 
 namespace webApp
 {
     public class Startup
     {
-        public IConfiguration AppConfig { get; set; }
-        public Startup(IConfiguration config)
-        {
-            AppConfig = config;
-        }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTelegramBot(AppConfig["botToken"]);
+            services.AddTelegramBot();
+            services.AddRouting();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, TelegramBotClient client)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, TelegramBotClient client, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            var routeBuilder = new RouteBuilder(app);
+            routeBuilder.MapPost("api/update", async context =>{
+                StreamReader sr = new StreamReader(context.Request.Body);
+                string body = await sr.ReadToEndAsync();
+                Telegram.Bot.Types.Update update = JsonConvert.DeserializeObject<Telegram.Bot.Types.Update>(body);
+                
+                switch(update.Type){
+                    case UpdateType.Message:
+                        await client.SendTextMessageAsync(update.Message.Chat.Id,
+                            "Просто ответ на сообщение", replyToMessageId: update.Message.MessageId);
+                        break;
+                    
+                    case UpdateType.EditedMessage:
+                        await client.SendTextMessageAsync(update.EditedMessage.Chat.Id,
+                            "Ответ на отредактированное сообщение", 
+                            replyToMessageId: update.EditedMessage.MessageId);
+                    break;
+                }
+                
+            });
+
+            app.UseRouter(routeBuilder.Build());
 
             app.Run(async context =>{
                 var me = await client.GetMeAsync();
